e5e01dfb0b043d0304266c225b3c87cadbd6846b
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-04 13:17:08 -0400

    Add navbar

--- layout/top.php
+++ layout/top.php
@@ -1,9 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
     <title><?= $title ?? "Paste" ?></title>
 </head>
+
 <body>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+                Paste
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary" href="/pastes/create.php">
+                            <strong>Create a paste</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
