b9f731e0a92e38b751179827c43b0ab6bb45ffb5
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-04 12:09:58 -0400

    Add form handler for paste creation

--- pastes/create.php
+++ pastes/create.php
@@ -2,9 +2,19 @@
 include "../layout/top.php";
 include "../db.php";
 
+function is_date_valid(string $date): bool
+{
+    $as_timestamp = strtotime($date);
+    if (!$as_timestamp) {
+        return false;
+    }
+
+    return date("Y-m-d", $as_timestamp) === $date;
+}
+
 function handle(): void
 {
-    global $conn, $err, $result;
+    global $conn, $err;
 
     if (!$_SERVER["REQUEST_METHOD"] === "POST") {
         $err = "Invalid request method.";
@@ -12,13 +22,35 @@ function handle(): void
         return;
     }
 
+    $content = $_POST["content"];
+    $expiry = $_POST["expires-at"];
 
+    if (empty($content) || (!empty($expiry) && !is_date_valid($expiry))) {
+        $err = "Invalid POST data.";
+        http_response_code(400);
+        return;
+    }
+
+    try {
+        $stmt = $conn->prepare("INSERT INTO `pastes` (`expires_at`, `content`) VALUES (:expiry, :content)");
+        $stmt->execute(["expiry" => $expiry ?: null, "content" => $content]);
+    } catch (PDOException $e) {
+        error_log("Could not create paste: $e");
+        $err = "Could not save paste.";
+        http_response_code(500);
+        return;
+    }
+
+    header("Location: /pastes/view.php?id={$conn->lastInsertId()}");
 }
 
 handle();
 ?>
 
 <h1>Create a paste</h1>
+<?php if ($err): ?>
+    <p><?= $err ?></p>
+<?php endif ?>
 <form action="/pastes/create.php" method="post">
     <div>
         <label for="content">Content</label>
