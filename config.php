f9eb5de490c024befdeb08a642705916e0e0976f
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-05-04 14:33:36 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    "title" => "Paste",
+    "db_url" => "localhost",
+    "db_port" => 3306,
+    "db_user" => "root",
+    "db_password" => "",
+    "db_name" => "paste",
+);
